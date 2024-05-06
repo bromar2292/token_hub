8080f07c5e6bbb64e47a69452a11b6b9c7d124a6
omarsultani <Omar.Sultani@mytwyn.com>
2024-05-06 21:17:13 +0100

    remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,3 @@ Future<void> main() async {
 
   runApp(const MyApp());
 }
-
-/// omar
-
-/// loggins
-/// omar makke repository interface in the domain layer
-/// presnetation shouldnt know about data layer
-/// use env
-
-/// make sure can download and run from other laptop
-/// make sure it works on iphone
